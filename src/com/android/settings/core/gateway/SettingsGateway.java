82fe889d099da39c9778e459b41340528943941a
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-21 19:30:11 +0800

    [RisingUI] Revamp personalization dashboard
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -428,7 +428,12 @@ public class SettingsGateway {
             PersonalizationsFragment.class.getName(),
             LockScreen.class.getName(),
             Themes.class.getName(),
-            Toolbox.class.getName()
+            Toolbox.class.getName(),
+            MonetSettings.class.getName(),
+            com.rising.settings.fragments.ui.fonts.FontPickerPreview.class.getName(),
+            Notifications.class.getName(),
+            Sound.class.getName(),
+            com.rising.settings.fragments.ui.IconShapes.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -478,6 +483,11 @@ public class SettingsGateway {
             Settings.PersonalizationsActivity.class.getName(),
             Settings.PersonalizationsLockscreenActivity.class.getName(),
             Settings.PersonalizationsToolboxActivity.class.getName(),
-            Settings.PersonalizationsThemesActivity.class.getName()
+            Settings.PersonalizationsThemesActivity.class.getName(),
+            Settings.PersonalizationsColorsActivity.class.getName(),
+            Settings.PersonalizationsFontsActivity.class.getName(),
+            Settings.PersonalizationsSoundsActivity.class.getName(),
+            Settings.PersonalizationsIconsActivity.class.getName(),
+            Settings.PersonalizationsNotificationsActivity.class.getName()
     };
 }
