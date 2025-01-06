10e5cefdba63c91e650addd5f19cc0ab78b469aa
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-06 22:03:44 +0800

    Make sure OEM parts gets high priority order

--- src/com/android/settings/dashboard/DashboardFragment.java
+++ src/com/android/settings/dashboard/DashboardFragment.java
@@ -78,6 +78,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
     public static final String CATEGORY = "category";
     private static final String TAG = "DashboardFragment";
     private static final long TIMEOUT_MILLIS = 50L;
+    
+    private int lastOrderIncrement = -1001;
 
     private static final List<String> ACCOUNT_INJECTED_KEYS = Arrays.asList(
     );
@@ -648,6 +650,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
                     } else if (SECURITY_PRIVACY_INJECTED_KEYS.contains(key)) {
                         group = screen.findPreference("top_level_security_privacy_category");
                     } else {
+                        KEY_ORDER.put(key, lastOrderIncrement);
+                        lastOrderIncrement = lastOrderIncrement - 1;
                         group = screen.findPreference("top_level_extras_category");
                     }
                     // Order the prefs within their respective category
